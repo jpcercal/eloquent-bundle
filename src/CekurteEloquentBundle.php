f88ddbe3afcd0f4ce4e05560d917072d810fb6d7
João Paulo Cercal <jpcercal@gmail.com>
2015-05-08 17:59:32 -0300

    Default structure to this bundle.

---
+++ src/CekurteEloquentBundle.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the Cekurte package.
+ *
+ * (c) João Paulo Cercal <jpcercal@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Cekurte\EloquentBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * CekurteEloquentBundle
+ *
+ * @author João Paulo Cercal <jpcercal@gmail.com>
+ */
+class CekurteEloquentBundle extends Bundle
+{
+}
